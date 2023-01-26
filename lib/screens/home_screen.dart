952fcf9a253673ba57e4ad6af09347de15f1be63
山本　堅心 <s2032149nb@s.chibakoudai.jp>
2023-01-26 10:58:33 +0900

    bottom_tabにおいてコードのリファクタリングを行った

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -3,8 +3,8 @@ import 'package:flutter/material.dart';
 import './health_management_screen.dart';
 import './drink_screen.dart';
 import './take_hand_screen.dart';
-import './memo_screen.dart';
 import '../widgets/text_to_speech.dart';
+import '../widgets/bottom_tab.dart';
 
 class HomeScreen extends StatelessWidget {
   static const routeName = '/home';
@@ -15,7 +15,6 @@ class HomeScreen extends StatelessWidget {
     final deviceHeight = MediaQuery.of(context).size.height -
         AppBar().preferredSize.height -
         MediaQuery.of(context).padding.top;
-    final deviceWidth = MediaQuery.of(context).size.width;
     final cardHeight = deviceHeight * 0.85 * 0.16;
 
     Widget onGenerateCard(
@@ -56,42 +55,6 @@ class HomeScreen extends StatelessWidget {
                       child: Icon(Icons.volume_up),
                     ),
                   ),
-                ],
-              ),
-            ),
-          ),
-        ),
-      );
-    }
-
-    Widget onGenerateTabCard(String titleText, VoidCallback tapFunction) {
-      return SizedBox(
-        height: deviceHeight * 0.1,
-        width: deviceWidth * 0.4,
-        child: Card(
-          elevation: 5,
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(10),
-          ),
-          child: InkWell(
-            onTap: () => tapFunction(),
-            onLongPress: () => tapFunction(),
-            child: Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 5),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Icon(Icons.abc), // TODO 画像の表示
-                  FittedBox(
-                    child: Text(
-                      titleText,
-                      style: TextStyle(
-                        fontSize: deviceHeight * 0.03,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                  ),
-                  const Icon(Icons.arrow_forward_ios),
                 ],
               ),
             ),
@@ -183,40 +146,9 @@ class HomeScreen extends StatelessWidget {
                 ],
               ),
             ),
-            Container(
-              height: deviceHeight * 0.12,
-              color: Colors.deepOrange[300],
-              child: Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 15),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    onGenerateTabCard('使い方', /*TODO 処理を追加*/ () {}),
-                    onGenerateTabCard('手書き', () {
-                      Navigator.of(context).pushNamedAndRemoveUntil(
-                          MemoScreen.routeName, (route) => false);
-                    }),
-                  ],
-                ),
-              ),
-            ),
-            Container(
-              height: deviceHeight * 0.03,
-              color: Colors.deepOrange[300],
-              child: const Align(
-                alignment: Alignment.bottomRight,
-                child: Padding(
-                  padding: EdgeInsets.symmetric(vertical: 5, horizontal: 35),
-                  child: Text(
-                    'produced by comecome',
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontWeight: FontWeight.bold,
-                      fontStyle: FontStyle.italic,
-                    ),
-                  ),
-                ),
-              ),
+            BottomTab(
+              receivedContext: context,
+              showTopPage: false,
             ),
           ],
         ),
